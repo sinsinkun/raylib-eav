29e21c588a722a6f8690265b1338a20b8af67175
Chang Xiao <chang.x1994@gmail.com>
2024-11-26 09:11:09 -0500

    separate db setup from init

--- eventloop.cpp
+++ eventloop.cpp
@@ -5,6 +5,28 @@
 
 using namespace App;
 
+// temp function for resetting db setup for testing
+void _setupDbTest(DbI::DbInterface* dbi) {
+  dbi->setup_tables();
+  dbi->new_blueprint("movies");
+  dbi->new_blueprint("books");
+  dbi->new_entity("Titanic", 1);
+  dbi->new_entity("1984", 2);
+  dbi->new_attr("genre", DbI::STR, true);
+  dbi->new_attr("length", DbI::FLOAT, false, "hrs");
+  dbi->new_attr("finished", DbI::BOOL, false);
+  dbi->new_ba_link(1, 1);
+  dbi->new_ba_link(1, 2);
+  dbi->new_ba_link(1, 3);
+  dbi->new_ba_link(2, 1);
+  dbi->new_ba_link(2, 3);
+  dbi->new_value(1, 1, "drama");
+  dbi->new_value(2, 1, "alt history");
+  dbi->new_value(1, 1, "romance");
+  dbi->new_value(1, 2, "1.25");
+  dbi->new_value(1, 3, "true");
+}
+
 void EventLoop::init() {
   // initialize assets
   dbInterface.init();
@@ -14,24 +36,7 @@ void EventLoop::init() {
     CloseWindow();
     return;
   }
-  // dbInterface.setup_tables();
-  // dbInterface.new_blueprint("movies");
-  // dbInterface.new_blueprint("books");
-  // dbInterface.new_entity("Titanic", 1);
-  // dbInterface.new_entity("1984", 2);
-  // dbInterface.new_attr("genre", DbI::STR, true);
-  // dbInterface.new_attr("length", DbI::FLOAT, false, "hrs");
-  // dbInterface.new_attr("finished", DbI::BOOL, false);
-  // dbInterface.new_ba_link(1, 1);
-  // dbInterface.new_ba_link(1, 2);
-  // dbInterface.new_ba_link(1, 3);
-  // dbInterface.new_ba_link(2, 1);
-  // dbInterface.new_ba_link(2, 3);
-  // dbInterface.new_value(1, 1, "drama");
-  // dbInterface.new_value(2, 1, "alt history");
-  // dbInterface.new_value(1, 1, "romance");
-  // dbInterface.new_value(1, 2, "1.25");
-  // dbInterface.new_value(1, 3, "true");
+  // _setupDbTest(&dbInterface);
   DbI::EavResponse res = dbInterface.get_entity_values(1);
   std::cout << "done" << std::endl;
 }
